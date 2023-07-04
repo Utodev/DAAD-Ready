6be3e4474f4224fb716ca7d215a2ad140f0b5632
Carlos Sanchez <csanchez71@gmail.com>
2023-07-04 10:46:53 +0200

    Control de versiones

---
+++ TOOLS/CHECKVER/checkver.php
@@ -0,0 +1,22 @@
+<?php
+
+$current_version = "0.8";
+
+
+$lvStr = $latest_version = file_get_contents("https://www.ngpaws.com/downloads/DAAD/DAADReady/LATEST_VERSION.TXT");
+if ($latest_version===false) $lvStr = 'Unable to connect to server';
+echo "DAAD Ready version (installed): $current_version\n";
+echo "DAAD Ready version (last): $lvStr\n";
+
+if ($latest_version===false) exit(0);
+
+if ($latest_version!=$current_version)
+{
+    echo "******************************************\n";
+    echo "**** NEW DAAD-READY VERSION AVAILABLE ****\n";
+    echo "******************************************\n";  
+    echo "\nPress enter to continue.\n";
+    $handle = fopen ("php://stdin","r");
+    $line = fgets($handle);
+    fclose($handle);
+}
