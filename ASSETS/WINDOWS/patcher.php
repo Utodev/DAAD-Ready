0c33e17c875d93d777756bc5578e20fa56e4f641
Carlos Sanchez <csanchez71@gmail.com>
2023-07-13 18:53:14 +0200

    New Windows resources

---
+++ ASSETS/WINDOWS/patcher.php
@@ -0,0 +1,22 @@
+<?php
+
+function makeGUID($trim=false)
+{
+    mt_srand((double)microtime() * 10000);
+    $charid = strtolower(md5(uniqid(rand(), true)));
+    $hyphen = chr(45);                  // "-"
+    $lbrace = $trim ? "" : chr(123);    // "{"
+    $rbrace = $trim ? "" : chr(125);    // "}"
+    $guidv4 = $lbrace.
+              substr($charid,  0,  8).$hyphen.
+              substr($charid,  8,  4).$hyphen.
+              substr($charid, 12,  4).$hyphen.
+              substr($charid, 16,  4).$hyphen.
+              substr($charid, 20, 12).
+              $rbrace;
+    return $guidv4;
+}
+
+$script = file_get_contents('ASSETS\WINDOWS\buildScript.iss');
+$GUID=makeGUID();
+file_put_contents('buildScript.tmp', str_replace('[[GUID', '{'.$GUID.'}', $script));
