c41c1a5d01632d030aacd8970ea44ea1ae7261c4
Uto <utodev@gmail.com>
2024-04-01 13:07:35 +0200

    version increased

--- TOOLS/CHECKVER/checkver.php
+++ TOOLS/CHECKVER/checkver.php
@@ -1,6 +1,6 @@
 <?php
 
-$current_version = "0.9.2";
+$current_version = "0.9.3";
 
 
 $lvStr = $latest_version = file_get_contents("https://www.ngpaws.com/downloads/DAAD/DAADReady/LATEST_VERSION.TXT");
