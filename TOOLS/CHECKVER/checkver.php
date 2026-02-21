5a4db39bc5607c75f1f671ced9e58c6f8a571948
Uto <utodev@gmail.com>
2026-02-21 13:38:22 +0100

    Check version updated to A2

--- TOOLS/CHECKVER/checkver.php
+++ TOOLS/CHECKVER/checkver.php
@@ -1,6 +1,6 @@
 <?php
 
-$current_version = "A1";
+$current_version = "A2";
 
 
 $lvStr = $latest_version = file_get_contents("https://www.ngpaws.com/downloads/DAAD/DAADReady/LATEST_VERSION_A_SERIES.TXT");
