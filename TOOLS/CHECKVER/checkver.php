b07b00e72e4c8349982ca5ea212082d129683eda
utodev <utodev@gmail.com>
2026-05-15 23:39:57 +0200

    Latest update to ADP target, just befor publishing

--- TOOLS/CHECKVER/checkver.php
+++ TOOLS/CHECKVER/checkver.php
@@ -1,13 +1,20 @@
 <?php
 
+define('WARNING_FILE','TOOLS\CHECKVER\checkver.warning');
 $current_version = "B";
+
+if (file_exists(WARNING_FILE))
+{
+    $times = file_get_contents(WARNING_FILE);
+    if ($times===false) $times = 0;
+    if ($times>3) exit(0);
+}
 
 
 $lvStr = $latest_version = file_get_contents("https://www.ngpaws.com/downloads/DAAD/DAADReady/LATEST_VERSION_A_SERIES.TXT");
 if ($latest_version===false) $lvStr = 'Unable to connect to server';
 echo "DAAD Ready version (installed): $current_version\n";
 echo "DAAD Ready version (last): $lvStr\n";
-
 if ($latest_version===false) exit(0);
 
 if ($latest_version!=$current_version)
@@ -20,3 +27,9 @@ if ($latest_version!=$current_version)
     $line = fgets($handle);
     fclose($handle);
 }
+
+if (file_exists(WARNING_FILE)) $times = file_get_contents(WARNING_FILE);
+    else $times = 0;
+
+$times++;
+file_put_contents(WARNING_FILE,$times);
